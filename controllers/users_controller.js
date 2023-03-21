62b05faeb3a6c10e5bf165c12fe16eae275ab6aa
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2023-03-21 09:46:17 +0530

    authentication and authorization

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,19 @@ const User= require('../models/user');
 
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile',{
-        title: "Home"
-    });
+    if (req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if (user){
+                return res.render('user_profile',{
+                    title:"User Profile",
+                    user:user
+                })
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 //Render the sign up page
@@ -44,5 +54,27 @@ module.exports.create = function(req,res){
 
 //get the sign in data and create the session for user
 module.exports.createSession = function(req,res){
-    //TODO later
+    //find the user
+    User.findOne({email : req.body.email}, function(err, user){
+        if (err){console.log('error in crating user while signing in'); return}
+
+        //handle user found
+        if (user){
+            //Handle password which doesn't match
+            if (user.password != req.body.password){
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookies('user_id', user.id);
+            return res.redirect('/users/profile');
+        }else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    });
+
+  
+
+    
+
 }
